b1dfa67999996ed84b4711d1076e6c9e99917ceb
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-03-23 14:19:44 +0100

    Adds vis. Updates css

--- components/articles.js
+++ components/articles.js
@@ -6,12 +6,13 @@ const config = require('../config')
 export default class Articles extends React.Component {
   render () {
     return <div>
-      <table>
+      <table className='fullWidth'>
         <thead>
           <tr>
             <th>Dager</th>
             <th>Plassering</th>
-            <th>Rediger</th>
+            <th>Vis</th>
+            <th>Endre</th>
           </tr>
         </thead>
         <tbody>
@@ -20,6 +21,7 @@ export default class Articles extends React.Component {
             <tr>
               <td>{line.age}</td>
               <td>{line.loc.replace(config.baseUrl, '')}</td>
+              <td><a href={line.loc} target='_blank'>Vis</a></td>
               <td><a href={line.loc.replace(config.baseUrl, `${config.baseUrl}/siteadmin`)} target='_blank'>Endre</a></td>
             </tr>
           )
