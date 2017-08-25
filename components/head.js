7d6c8858d0b04c29320cf76537694dc9970bc6b4
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-08-25 08:22:20 +0200

    Updates Node. Updates dependemcies

--- components/head.js
+++ components/head.js
@@ -8,7 +8,7 @@ export default () => (
     <Head>
       <title>Utdaterte artikler</title>
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      <link href='//cdn.muicss.com/mui-0.9.18/css/mui.min.css' rel='stylesheet' type='text/css' media='screen' />
+      <link href='//cdn.muicss.com/mui-0.9.22/css/mui.min.css' rel='stylesheet' type='text/css' media='screen' />
       <link href='static/css/app.css' rel='stylesheet' type='text/css' media='screen' />
       <link rel='icon' sizes='192x192' href='static/images/icons/chrome-touch-icon-192x192.png' />
       <link rel='apple-touch-icon' href='static/images/icons/apple-touch-icon-precomposed.png' />
