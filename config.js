001936c3875478df1372d41a822b3235eb85aa7e
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-10-11 11:23:24 +0200

    Update dependencies. Changes rules

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict'
 
 module.exports = {
-  sitemapUrl: process.env.SITEMAP_URL || 'https://sitemap.t-fk.win?sitemap=https://www.telemark.no/sitemap.xml',
+  sitemapUrl: process.env.SITEMAP_URL || 'https://sitemap.microservice.click?sitemap=https://www.telemark.no/sitemap.xml',
   baseUrl: process.env.BASE_URL || 'https://www.telemark.no'
 }
